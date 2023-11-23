6473732ad18463a7fddb56433e2993f23a9e5080
bezdariia <theandrdash@gmail.com>
2023-11-23 20:40:44 +0900

    added 4_log.py

---
+++ 4_log.py
@@ -0,0 +1,90 @@
+# операции сравнивания
+
+x = -10
+y =  10
+
+# операция равенства
+result = x == y
+
+# операция неравенства
+result = x != y
+
+# операция  меньше
+result = x < y
+
+# операция  больше
+result = x > y
+
+# операция  меньше либо равно
+x = 'i love penis'
+y = 'I love penis' 
+result = x == y
+
+# print(result)
+
+
+# логические операции
+v0 = False
+v1 = True
+
+# оператор НЕ (инвертировать)
+result =  not v1
+
+# И (конъюнкция, логическое умножение), тру только если оба пер. тру
+result = v0 and v1
+
+# или (дизъюнкция, логическое сложение), фолс только если оба фолс
+result = v0 or v1
+
+
+# print(result)
+
+# условные операции
+
+# a = int(input('введите число: ')) 
+# b = 0
+
+# # IF
+# if a > 300:
+#     print('иди нахуй')
+# if a < 300:
+#     print('иди в жопку') 
+# if a == 300:
+    # print('отсоси у тракториста')
+
+
+# задача
+# temp = 36
+# target = 36
+
+# state = 'on'
+
+# condition = (temp < target) and (state == 'on')
+
+# if condition:
+#     state = 'on'
+# # оператор else (когда предыдущее условие не срабатывает)
+# else:
+#     state = 'off' 
+# print(state)
+
+
+# оператор  elif (else if)
+# a = 0
+
+# if a < 0:
+#     print('отрицательное число')
+# elif a > 0:
+#     print('положительное число')
+# elif a == 0:
+#     print('это золотая середина')
+
+count = 1
+
+if count < 10:
+    print('A')
+if count < 20:
+    print('B')
+if count < 30:
+    print('C')
+
